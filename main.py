271e5c9c5bd77efbf566bb7de15dba9071dccae4
Jacob <wolstenholme.jacob@gmail.com>
2019-08-03 17:08:12 +1200

    Add parsing of lowercase bans and number of warmups

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import aiohttp
 from settings import *
 
 client = discord.Client()
-resultpattern = re.compile('(https://osu.ppy.sh/community/matches/)|(https://osu.ppy.sh/mp/)(?P<id>[0-9]+) (?P<b1>(NM|HD|HR|DT|FM)[0-9]) (?P<b2>(NM|HD|HR|DT|FM)[0-9])')
+resultpattern = re.compile('(https://osu.ppy.sh/community/matches/)|(https://osu.ppy.sh/mp/)(?P<id>[0-9]+) (?P<b1>(NM|nm|HD|hd|HR|hr|DT|dt|FM|fm)[0-9]) (?P<b2>(NM|nm|HD|hd|HR|hr|DT|dt|FM|fm)[0-9])(?P<wm>( [0-9]))?')
 
 
 @client.event
@@ -33,6 +33,12 @@ async def handle_match(message):
     p1b = group.group('b1')
     p2b = group.group('b2')
     
+    warmups = group.group('wm')
+    if warmups is None:
+        warmups = 2
+    else:
+        warmups = int(warmups)
+    
     lobbyjson = await request(f'https://osu.ppy.sh/api/get_match?k={apiKey}&mp={lobby_id}')
     
     # Get player usernames from lobby title
@@ -44,7 +50,7 @@ async def handle_match(message):
     ids_to_usernames = {}
     
     # List slicing skips warmups
-    games = lobbyjson['games'][2:]
+    games = lobbyjson['games'][warmups:]
     
     # Store username -> tourney match score
     finalscore = {p1: 0, p2: 0}
@@ -63,8 +69,11 @@ async def handle_match(message):
             else:
                 username = ids_to_usernames[userid]
             
-            mapscores[username] = int(score['score'])
-        
+            if score['pass'] == '1':
+                mapscores[username] = int(score['score'])
+            else:
+                mapscores[username] = 0
+
         if mapscores[p1] > mapscores[p2]:
             finalscore[p1] += 1
         else:
@@ -74,9 +83,9 @@ async def handle_match(message):
               ':keycap_ten: ']
     embed = discord.Embed(title=f'{tourneyRound} - https://osu.ppy.sh/mp/{lobby_id}', color=0xe47607)
     embed.set_author(name=f'{acronym}')
-    embed.add_field(name=f'{p1} {emotes[finalscore[p1]]}', value=f'Banned {p1b}', inline=True)
-    embed.add_field(name=f'{emotes[finalscore[p2]]} {p2}', value=f'Banned {p2b}', inline=True)
-    embed.set_footer(text=f'Coded by Diony | Reported by {message.author}')
+    embed.add_field(name=f'{p1} {emotes[finalscore[p1]]}', value=f'Banned {p1b.upper()}', inline=True)
+    embed.add_field(name=f'{emotes[finalscore[p2]]} {p2}', value=f'Banned {p2b.upper()}', inline=True)
+    embed.set_footer(text=f'Coded by Diony | Reported by {message.author.display_name}')
     await message.delete()
     await message.channel.send(embed=embed)
 
