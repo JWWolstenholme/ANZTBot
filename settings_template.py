29c56c07600f5b4df14615ad5b473137f1ba9458
Jacob <wolstenholme.jacob@gmail.com>
2020-02-23 08:19:05 +1300

    Finally update settings_template.py

--- settings_template.py
+++ settings_template.py
@@ -1,5 +1,20 @@
+# Discord bot token
 botToken = ''
+# Which twitch channel to monitor for changing the bot's presence and announcing streams
+twitchchannel = 'osuanzt'
+# Osu! api key
 apiKey = ''
-
+# Shown to users to signify round in results
 tourneyRound = ''
-acronym = ''
+# The name of the sheet within the ANZT7S spreadsheet that contains this week's schedule
+schedule_sheet_name = ''
+# Twitch application client ID for making api calls
+clientID = ''
+# A list of referee Osu! user ids to ignore when looking at Osu! lobby scores
+# This only matter if the reffs play and actually beat one of the players
+referees = []
+# A dict of Osu! beatmap ids to shortened mod pool. e.g. 725026: 'NM1'
+pool = {}
+# A dict of Osu! usernames to ISO flag codes
+# Yes, I know this is bad
+country = {}
