aab8d82960a86b37487909cedc795bc3c3b04a33
JWWolstenholme <13296453+JWWolstenholme@users.noreply.github.com>
2021-03-31 06:31:06 +1300

    Update tourney-signup.py to use new json settings Also add new settings to settings_template.json

--- cogs/tourney-signup.py
+++ cogs/tourney-signup.py
@@ -6,8 +6,7 @@ from discord import Embed, User
 from discord.ext import commands
 from discord.ext.commands.converter import MessageConverter
 from discord.ext.commands.errors import MessageNotFound
-from settings import *
-from utility_funcs import res_cog
+from utility_funcs import get_setting, res_cog
 
 
 class TourneySignupCog(commands.Cog):
@@ -18,7 +17,7 @@ class TourneySignupCog(commands.Cog):
         self.prompted_users = []
 
     async def _connpool(self):
-        return await (await res_cog(self.bot)).connpool()
+        return await res_cog(self.bot).connpool()
 
     @commands.command()
     async def register(self, ctx):
@@ -66,14 +65,14 @@ class TourneySignupCog(commands.Cog):
                 record = await conn.fetchrow('select * from settings')
                 self.watch_message_id = record['watch_message_link']
 
-    async def generate_oauth_url(self, user_id: int):
+    async def generate_oauth_url(self, user_id: int, settings):
         user_id = str(user_id).encode()
-        user_id_enc = Fernet(key).encrypt(user_id)
+        user_id_enc = Fernet(settings["key"].encode()).encrypt(user_id)
         return (
             f'https://osu.ppy.sh/oauth/authorize?'
-            f'client_id={osu_app_client_id}&'
+            f'client_id={settings["osu_app_client_id"]}&'
             f'response_type=code&'
-            f'redirect_uri={redirect_url}&'
+            f'redirect_uri={settings["redirect_url"]}&'
             f'state={user_id_enc.decode()}'
         )
 
@@ -81,11 +80,12 @@ class TourneySignupCog(commands.Cog):
         if await self.user_prompted(user.id):
             return False
 
-        oauth_url = await self.generate_oauth_url(user.id)
+        setts = get_setting("tourney-signup")
+        oauth_url = await self.generate_oauth_url(user.id, setts)
         colour = 0xf5a623
 
         embed = Embed(title="Click here to register for ANZT8W", colour=colour, url=oauth_url)
-        embed.set_footer(text=f"This link is unique to you - Registrations close {signup_close_date}")
+        embed.set_footer(text=f"This link is unique to you - Registrations close {setts['signup_close_date']}")
         await user.send(embed=embed)
 
         embed = Embed(colour=colour,
@@ -122,13 +122,14 @@ class TourneySignupCog(commands.Cog):
         addr = writer.get_extra_info('peername')
         print(f"Received {data!r} from {addr!r}")
 
-        session = await (await res_cog(self.bot)).session()
+        session = await res_cog(self.bot).session()
+        setts = get_setting("tourney-signup")
 
         # See https://osu.ppy.sh/docs/index.html?bash#authorization-code-grant to see what the rest of this method is doing.
         code = data['code']
         state = data['state']
         try:
-            state = Fernet(key).decrypt(state.encode()).decode()
+            state = Fernet(setts["key"].encode()).decrypt(state.encode()).decode()
         except InvalidToken:
             await self.write(writer, False, "'State' parameter is bad: try asking ANZT bot for another link.")
             return
@@ -139,11 +140,11 @@ class TourneySignupCog(commands.Cog):
 
         print(f'Using one-time code to get authorization token')
         data = {
-            'client_id': osu_app_client_id,
-            'client_secret': osu_app_client_secret,
+            'client_id': setts["osu_app_client_id"],
+            'client_secret': setts["osu_app_client_secret"],
             'code': code,
             'grant_type': 'authorization_code',
-            'redirect_uri': redirect_url
+            'redirect_uri': setts["redirect_url"]
         }
         # There's duplicate code here but idk how to elegantly fix that
         async with session.post('https://osu.ppy.sh/oauth/token', data=data) as r:
@@ -164,7 +165,7 @@ class TourneySignupCog(commands.Cog):
             json = await r.json()
             user_country = json['country_code']
             user_id = json['id']
-        if user_country not in allowed_countries:
+        if user_country not in setts["allowed_countries"]:
             print('user rejected due to their flag')
             await self.write(writer, False, 'You need an Australian or New Zealand flag on your profile!')
             return
@@ -189,7 +190,7 @@ class TourneySignupCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         # Wait for error cog to be ready
-        await asyncio.sleep(1)
+        await asyncio.sleep(5)
         await self.load_from_settings()
 
         server = await asyncio.start_server(self.handler, '127.0.0.1', 7865)
