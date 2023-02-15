3b92247bbcd82768b870991d48213e8299999d2c
JWWolstenholme <13296453+JWWolstenholme@users.noreply.github.com>
2023-02-16 01:53:18 +1300

    Implement ability to pick two amplifiers per user

--- cogs/amplifiers.py
+++ cogs/amplifiers.py
@@ -1,4 +1,5 @@
 import asyncio
+from enum import Enum
 
 import discord
 from discord.app_commands import MissingPermissions
@@ -7,53 +8,79 @@ from discord.ext import commands
 from utility_funcs import get_setting, res_cog
 
 
-class AmplifierDropdown(discord.ui.Select):
+class AmplifierEnum(Enum):
+    ACTIVE = 1
+    PASSIVE = 2
+
+
+class ActiveAmplifierDropdown(discord.ui.Select):
     def __init__(self):
         options = [
-            discord.SelectOption(label='Amplifier Option One', description='First amplifier option provided above', emoji='1️⃣', value=0),
-            discord.SelectOption(label='Amplifier Option Two', description='Second amplifier option provided above', emoji='2️⃣', value=1),
-            discord.SelectOption(label='Amplifier Option Three', description='Third amplifier option provided above', emoji='3️⃣', value=2),
-            discord.SelectOption(label='Lucky Dip', description='Give me a random amplifier not listed above', emoji='🎲', value=3)
+            discord.SelectOption(label='Active Amplifier Option One', description='First active amplifier option provided above', emoji='1️⃣', value=0),
+            discord.SelectOption(label='Active Amplifier Option Two', description='Second active amplifier option provided above', emoji='2️⃣', value=1),
+            discord.SelectOption(label='Lucky Dip', description='Give me a random active amplifier not listed above', emoji='🎲', value=2)
         ]
-        super().__init__(placeholder='Pick one amplifier...', min_values=1, max_values=1, options=options, custom_id='amplifier-select-persistent')
+        super().__init__(placeholder='Pick one active amplifier...', min_values=1, max_values=1, options=options, custom_id='active-amplifier-select-persistent')
 
     async def callback(self, interaction: discord.Interaction):
-        selected_option = int(self.values[0])
+        await shared_callback(self, interaction, AmplifierEnum.ACTIVE)
 
-        await interaction.response.defer(thinking=True)
 
-        connpool = await res_cog(interaction.client).connpool()
+class PassiveAmplifierDropdown(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label='Passive Amplifier Option One', description='First Passive amplifier option provided above', emoji='1️⃣', value=0),
+            discord.SelectOption(label='Passive Amplifier Option Two', description='Second Passive amplifier option provided above', emoji='2️⃣', value=1),
+            discord.SelectOption(label='Lucky Dip', description='Give me a random passive amplifier not listed above', emoji='🎲', value=2)
+        ]
+        super().__init__(placeholder='Pick one passive amplifier...', min_values=1, max_values=1, options=options, custom_id='passive-amplifier-select-persistent')
 
-        week_no = get_setting('amplifiers', 'week_number')
+    async def callback(self, interaction: discord.Interaction):
+        await shared_callback(self, interaction, AmplifierEnum.PASSIVE)
 
-        async with connpool.acquire() as conn:
-            async with conn.transaction():
-                # Convert user's discord id to their osu! ID.
-                osu_id = int(await conn.fetchval('''select osu_id from players where discord_id=$1;''', interaction.user.id))
-                # Reset their current picks TODO: Make sure this doesn't mess up future weeks where there will be multiple picks
-                await conn.execute('''update players_amplifiers set is_picked=False where osu_id=$1 and week=$2;''', osu_id, week_no)
-                # Convert the users picked option to it's corresponding amplifier
-                options = await conn.fetch('''
-                    select pa.amplifier_id, amplifier_name from players_amplifiers as pa
-                        natural left join players
-                        left join amplifiers as a on pa.amplifier_id = a.amplifier_id
-                        where discord_id=$1
-                        and pa.week=$2
-                        order by amplifier_id asc;''', interaction.user.id, week_no)
-                selected_amplifier_record = options[selected_option]
-                # Set the picked amplifiers as picked
-                await conn.execute('''update players_amplifiers set is_picked=True where osu_id=$1 and amplifier_id=$2 and week=$3;''', osu_id, selected_amplifier_record['amplifier_id'], week_no)
 
-        if selected_amplifier_record['amplifier_id'] == 777:
-            await interaction.followup.send(f'You\'ve picked "{selected_amplifier_record["amplifier_name"]}". This will draw you a new amplifier not listed above once the selection period is over.')
-        else:
-            await interaction.followup.send(f'You\'ve picked the amplifier "{selected_amplifier_record["amplifier_name"]}"')
+async def shared_callback(self, interaction: discord.Interaction, amplifier_type: AmplifierEnum):
+    selected_option = int(self.values[0])
+
+    is_active = amplifier_type == AmplifierEnum.ACTIVE
+
+    await interaction.response.defer(thinking=True)
+
+    connpool = await res_cog(interaction.client).connpool()
+
+    week_no = get_setting('amplifiers', 'week_number')
+
+    async with connpool.acquire() as conn:
+        async with conn.transaction():
+            # Convert user's discord id to their osu! ID.
+            osu_id = int(await conn.fetchval('''select osu_id from players where discord_id=$1;''', interaction.user.id))
+            # Reset their current picks TODO: Make sure this doesn't mess up future weeks where there will be multiple picks
+            await conn.execute('''update players_amplifiers set is_picked=False where osu_id=$1 and week=$2 and amplifier_id in
+                                    (select amplifier_id from amplifiers where is_active=$3);''', osu_id, week_no, is_active)
+            # Convert the users picked option to it's corresponding amplifier
+            options = await conn.fetch('''
+                select pa.amplifier_id, amplifier_name from players_amplifiers as pa
+                    natural left join players
+                    left join amplifiers as a on pa.amplifier_id = a.amplifier_id
+                    where discord_id=$1
+                    and is_active=$3
+                    and pa.week=$2
+                    order by amplifier_id asc;''', interaction.user.id, week_no, is_active)
+            selected_amplifier_record = options[selected_option]
+            # Set the picked amplifiers as picked
+            await conn.execute('''update players_amplifiers set is_picked=True where osu_id=$1 and amplifier_id=$2 and week=$3;''', osu_id, selected_amplifier_record['amplifier_id'], week_no)
+
+    if selected_amplifier_record['amplifier_id'] in [777, 778]:
+        await interaction.followup.send(f"You've picked the {'active' if is_active else 'passive'} amplifier \"{selected_amplifier_record['amplifier_name']}\". This will draw you a new {'active' if is_active else 'passive'} amplifier not listed above once the selection period is over.")
+    else:
+        await interaction.followup.send(f"You've picked the {'active' if is_active else 'passive'} amplifier \"{selected_amplifier_record['amplifier_name']}\"")
 
 
 class AmplifierDropdownView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(AmplifierDropdown())
+        self.add_item(PassiveAmplifierDropdown())
+        self.add_item(ActiveAmplifierDropdown())
 
     def on_error(self, interaction, error, item):
         # TODO: Handle these errors better. Ideally incorperate interaction errors with the error-reporting cog.
@@ -85,7 +112,7 @@ class AmplifiersCog(commands.Cog):
     @discord.app_commands.checks.has_permissions(administrator=True)
     async def request_pick(self, interaction: discord.Interaction, discord_ids: str, delay_seconds: int):
         ids = discord_ids.split('|')
-        await interaction.response.defer(thinking=True, ephemeral=True)
+        await interaction.response.defer(thinking=True, ephemeral=True)  # This defer only lasts 15 minutes which this loop can often exceed
         for i in ids:
             await self.send_amplifier_options(int(i))
             await asyncio.sleep(delay=delay_seconds)
@@ -105,19 +132,38 @@ class AmplifiersCog(commands.Cog):
         week_no = get_setting('amplifiers', 'week_number')
 
         async with (await self._connpool()).acquire() as conn:
-            options = await conn.fetch('''
+            passive_options = await conn.fetch('''
                 select pa.amplifier_id, amplifier_name from players_amplifiers as pa
                     natural left join players
                     left join amplifiers as a on pa.amplifier_id = a.amplifier_id
                     where discord_id=$1
+                    and is_active=false
+                    and pa.week=$2
+                    order by amplifier_id asc;''', member.id, week_no)
+            active_options = await conn.fetch('''
+                select pa.amplifier_id, amplifier_name from players_amplifiers as pa
+                    natural left join players
+                    left join amplifiers as a on pa.amplifier_id = a.amplifier_id
+                    where discord_id=$1
+                    and is_active=true
                     and pa.week=$2
                     order by amplifier_id asc;''', member.id, week_no)
 
-        preamble = f'Hello! Your options for amplifiers this week are:\n\n'
-        for i, option in enumerate(options):
+        preamble = f'Hello! This week we need you to pick both a passive and an active amplifier.\n'
+
+        preamble += '\nYour **passive** amplifier options for this week are:\n'
+        for i, option in enumerate(passive_options):
             emote = ":game_die:" if option['amplifier_name'] == "Lucky Dip" else f":{self.discord_numbers[i+1]}:"
             preamble += f'{emote} - "{option["amplifier_name"]}"\n'
-        preamble += "\nRefer to the main sheet for amplifier descriptions (<https://bit.ly/ANZT10SAmplifierDescriptions>)\nYou have until 11:59pm Wednesday 1st (AEDT) to pick one of the above amplifiers or one of them will be selected at random (except lucky dip).\nPlease also note, if you reschedule your match to prior to 6am Thursday 2nd (AEDT), your amplifiers must be locked in 6 hours prior to the match."
+
+        preamble += "\nYour **active** amplifier options for this week are:\n"
+        for i, option in enumerate(active_options):
+            emote = ":game_die:" if option['amplifier_name'] == "Lucky Dip" else f":{self.discord_numbers[i+1]}:"
+            preamble += f'{emote} - "{option["amplifier_name"]}"\n'
+
+        preamble += """\nRefer to the main sheet for amplifier descriptions (<https://bit.ly/ANZT10SAmplifierDescriptions>)
+You have until 11:59pm Thursday 16th (AEDT) to pick one of the above amplifiers or one of them will be selected at random (except lucky dip).
+Please also note, if you reschedule your match to prior to 6am Friday 17th (AEDT), your amplifiers must be locked in 6 hours prior to the match."""
         try:
             await member.send(preamble, view=view)
             await self.bot_log(f"✅ Sent amplifier options to user with discord_id: {discord_id}, tag: {member.name}#{member.discriminator}")
